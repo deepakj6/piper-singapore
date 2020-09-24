febe481c9fd4a3070ba1ade2aee2fcf4e742932a
DJ <deepakj6@yahoo.co.in>
2020-09-24 03:25:04 +0000

    Lab 1 - Day 2

--- app.py
+++ app.py
@@ -7,30 +7,19 @@
 ##################################################
 
 # Import modules required for app
-import os
-from flask import Flask, render_template, request
-from models import get_photos, insert_photo, upload_photo
+from flask import Flask
 
 # Create a Flask instance
 app = Flask(__name__)
 
 ##### Define routes #####
-@app.route('/')
-def home():
-    album_photos = get_photos()							# Call function in 'models.py' to retrieve all photo's from database
-    return render_template('default.html',album_photos=album_photos,url="home")
+@app.route("/")
+def index():
+    html = """<h3>Hello World!</h3><br>
+            This is a basic web page written in Python and served by Flask
+            """.format()
+    return html
 
-# This route accepts GET and POST calls
-@app.route('/upload', methods=['POST'])
-def upload():
-	insert_photo(request)								# Call function in 'models.py' to process the database transaction
-	upload_photo(request.files['photo'])				# Call function in 'models.py' to upload photo to ECS
-	return render_template('submit-photo.html')			# Return a page to inform the user of a successful upload
-
-@app.route('/photo/<path:photo>')
-def photo(photo):
-    return render_template('photo.html',photo=photo)
-
-##### Run the Flask instance, browse to http://localhost:5000 #####
+##### Run the Flask instance, browse to http://<< Host IP or URL >>:port #####
 if __name__ == "__main__":
-	app.run(debug=False, host='0.0.0.0', port=int(os.getenv('PORT','5000')), threaded=True)
+    app.run(debug=False, host='0.0.0.0', port='7000', threaded=True)
